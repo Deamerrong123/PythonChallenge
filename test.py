30f4d489203a2c0d661c695d1f5c8d2a82d0ae7a
Rong <deamerrong123@gmail.com>
2019-04-10 19:20:34 -0400

    update test.py

--- test.py
+++ test.py
@@ -3,5 +3,12 @@ from first import firstDuplicate
 a = [2,3,1,4,5,3,4,2]
 
 print(firstDuplicate(a))
+print("------------------")
+print("Should print out 4")
+print
 
-
+b = [1,1,2,2,1]
+print(firstDuplicate(b))
+print("-------------------")
+print("Shoule print out 1")
+print()
