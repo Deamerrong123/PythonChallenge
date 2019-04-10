31f500596ed3928b08b612240f882a14160e8af7
q.rong <q.rong@bc.baruch.cuny.edu>
2019-04-10 14:17:48 -0400

    change first.py

--- first.py
+++ first.py
@@ -1,14 +1,6 @@
 def firstDuplicate(a):
         Index = []
         for i in range(len(a)-1):
-            print(a[i],end = " ")
-
-
-    
-
-
-
-
-            
-
-
+            if a[i:].count(a[i]) > 1:
+		Index.append((a[i+1:].index(a[i]),str(a[i]))
+		
